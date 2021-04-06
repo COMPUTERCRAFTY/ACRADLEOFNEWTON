e3e36de534d5234bee35f2e32d15b8b7c6f5570f
COMPUTERCRAFTY <77259844+COMPUTERCRAFTY@users.noreply.github.com>
2021-04-06 20:50:29 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,23 @@
+class Bob{
+    constructor(x, y) {
+      var options = {
+          isStatic:false,
+          'restitution':1,
+          'friction':0,
+          'density':7.8
+      }
+      this.x = x;
+      this.y = y;
+      this.body = Bodies.circle(this.x, this.y, 25, options)
+      World.add(world, this.body);
+      console.log(this.body) 
+    }
+    display(){
+      push();
+      ellipseMode(RADIUS);
+      fill(254,0,255);
+      ellipse(this.body.position.x, this.body.position.y, 25, 25);
+      pop();
+      
+    }
+  };
